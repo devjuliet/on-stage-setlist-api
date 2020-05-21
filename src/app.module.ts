e5dc19025dda87209827b2b3d9f25ef36e941025
Luismiguel Ortiz <luismi.luu@gmail.com>
2020-05-21 01:31:55 -0600

    Funcionalidad del login y registro habilitadas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './modules/user/user.module';
-//import { AuthModule } from './modules/auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { CatalogsModule } from './modules/catalogs/catalogs.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, /*AuthModule,*/ CatalogsModule],
+  imports: [DatabaseModule, UserModule, AuthModule, CatalogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
