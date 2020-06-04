e4841f40d33c61ad0c120f277ef95d6b5fa135f5
Julieta <juliiy98@gmail.com>
2020-06-04 15:57:21 -0600

    quick fix

--- src/modules/band-members/band-members.service.ts
+++ src/modules/band-members/band-members.service.ts
@@ -14,105 +14,104 @@ import { LiveDesigner } from '../../models/live-designers.entity';
 
 @Injectable()
 export class BandMembersService {
-    constructor(
-        @Inject('BandRepository')
-        private readonly bandRepository: typeof Band,
-        @Inject('LiveDesignerRepository')
-        private readonly liveDesignerRepository: typeof LiveDesigner,
-        @Inject('BandMemberRepository')
-        private readonly bandMemberRepository: typeof BandMember,
-        @Inject('LiveEventRepository')
-        private readonly liveEventRepository: typeof LiveEvent,
-        @Inject('BandGenreRepository')
-        private readonly bandGenreRepository: typeof BandGenre,
-        @Inject('UserRepository')
-        private readonly userRepository: typeof User,
-        @Inject('UserHistoryRepository')
-        private readonly userHistoryRepository: typeof UserHistory,
-        @Inject('SetRepository')
-        private readonly setRepository: typeof Set,
-        @Inject('SongRepository')
-        private readonly songRepository: typeof Song,
-    ) { }
+  constructor(
+    @Inject('BandRepository')
+    private readonly bandRepository: typeof Band,
+    @Inject('LiveDesignerRepository')
+    private readonly liveDesignerRepository: typeof LiveDesigner,
+    @Inject('BandMemberRepository')
+    private readonly bandMemberRepository: typeof BandMember,
+    @Inject('LiveEventRepository')
+    private readonly liveEventRepository: typeof LiveEvent,
+    @Inject('BandGenreRepository')
+    private readonly bandGenreRepository: typeof BandGenre,
+    @Inject('UserRepository')
+    private readonly userRepository: typeof User,
+    @Inject('UserHistoryRepository')
+    private readonly userHistoryRepository: typeof UserHistory,
+    @Inject('SetRepository')
+    private readonly setRepository: typeof Set,
+    @Inject('SongRepository')
+    private readonly songRepository: typeof Song,
+  ) {}
 
-    //find song by id, member and song belongs to same band id
-    async findSongById(songId: number, userId): Promise<ServerMessages> {
-        try {
-            const song = await this.songRepository.findOne({
-                where: { idSong: songId },
-            });
-            if (song == null) {
-                return new ServerMessages(false, 'Song id ' + songId, {});
-            }
+  //find song by id, member and song belongs to same band id
+  async findSongById(songId: number, userId): Promise<ServerMessages> {
+    try {
+      const song = await this.songRepository.findOne({
+        where: { idSong: songId },
+      });
+      if (song == null) {
+        return new ServerMessages(false, 'Song id ' + songId, {});
+      }
 
-            let bandId = song.idBand;
-            console.log(bandId)
-            const bandMember = await this.bandMemberRepository.findOne({
-                where: {
-                    idBand: bandId,
-                    [Op.and]: [{ idUser: userId }],
-                }
-            });
+      let bandId = song.idBand;
+      console.log(bandId);
+      const bandMember = await this.bandMemberRepository.findOne({
+        where: {
+          idBand: bandId,
+          [Op.and]: [{ idUser: userId }],
+        },
+      });
 
-            if (bandMember != null) {
-                return new ServerMessages(false, 'Song id ' + songId, song);
-            } else {
-                return new ServerMessages(false, 'You dont belong to the band to see this song', {});
-            }
-
-        } catch (error) {
-            return new ServerMessages(true, 'Error ocurred', error);
-        }
+      if (bandMember != null) {
+        return new ServerMessages(false, 'Song id ' + songId, song);
+      } else {
+        return new ServerMessages(
+          false,
+          'You dont belong to the band to see this song',
+          {},
+        );
+      }
+    } catch (error) {
+      return new ServerMessages(true, 'Error ocurred', error);
     }
+  }
 
-    async findAllEventsByUserId(userId): Promise<ServerMessages> {
-        //encontrar todas la bands que pertenece el usuario
-        let allEvents = [];
-        try {
-            const bands = await this.bandMemberRepository.findAll({
-                attributes: ['idBand'],
-                where: { idUser: userId },
+  async findAllEventsByUserId(userId): Promise<ServerMessages> {
+    //encontrar todas la bands que pertenece el usuario
+    const eventsOfMember = [];
+    try {
+      const bands = await this.bandMemberRepository.findAll({
+        attributes: ['idBand'],
+        where: { idUser: userId },
+      });
 
+      //encontrar todos los eventos por banda
+      return Promise.all(
+        bands.map(band =>
+          this.liveEventRepository.findAll({
+            where: { idBand: band.idBand },
+          }),
+        ),
+      )
+        .then(allEvents => {
+          allEvents.forEach(eventsPerBand => {
+            eventsPerBand.forEach(event => {
+              eventsOfMember.push(event);
             });
-
-            //encontrar todos los eventos por banda
-            return Promise.all(
-                bands.map(band =>
-                    this.liveEventRepository.findAll({
-                        where: { idBand: band.idBand },
-                    }),
-                ),
-            ).then(events => {
-                if (events.length != 0) {
-                    allEvents.push(events)
-                }
-
-            }).then(
-                //regresar eventos
-                () => new ServerMessages(false, 'Songs not saved', allEvents)
-            )
-
-        } catch (error) {
-            console.log(error);
-            return new ServerMessages(true, 'Error ocurred', error);
-        }
-
+          });
+        })
+        .then(
+          //regresar eventos
+          () => new ServerMessages(false, 'Songs not saved', eventsOfMember),
+        );
+    } catch (error) {
+      console.log(error);
+      return new ServerMessages(true, 'Error ocurred', error);
     }
+  }
 
-    async getBandMemberHistory(userId): Promise<ServerMessages> {
-        try {
-            const history = await this.userHistoryRepository.findAll({
-                where: { idUser: userId },
+  async getBandMemberHistory(userId): Promise<ServerMessages> {
+    try {
+      const history = await this.userHistoryRepository.findAll({
+        where: { idUser: userId },
+      });
 
-            });
-
-            return new ServerMessages(true, 'History of user ' + userId, history);
-
-        } catch (error) {
-            console.log(error);
-            return new ServerMessages(true, 'Error ocurred', error);
-        }
+      return new ServerMessages(true, 'History of user ' + userId, history);
+    } catch (error) {
+      console.log(error);
+      return new ServerMessages(true, 'Error ocurred', error);
     }
-
-
+  }
 }
